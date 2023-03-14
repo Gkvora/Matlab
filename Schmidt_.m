12cfd643017b8a3518df67125ccd72c9b3202603
GAUTAM VORA <117502951+Gkvora@users.noreply.github.com>
2023-03-14 03:06:41 -0700

    Add files via upload

---
+++ Schmidt_.m
@@ -0,0 +1,80 @@
+clear;clc;
+h=input('enter value of h');
+k=input('enter value of k');
+c=input('enter value of c');
+w=(c^2*k)/h^2;
+x=0:h:1;
+t=0:k:1;
+m=length(x);
+n=length(t);
+u=zeros(m,n);
+IC=sin(pi*x);
+u(:,1)=IC';
+u([1,end],:)=0;
+for j=1:n-1
+    for i=2:m-1
+        u(i,j+1)=w*u(i+1,j)+(1-2*w)*u(i,j)+w*u(i-1,j);
+    end
+end
+u
+surf(u);
+xlabel('x');
+ylabel('t');
+zlabel('u');
+title('Heat Equation');
+
+
+% clear;clc;
+% h=input('enter value of h');
+% k=input('enter value of k');
+% c=input('enter value of c');
+% w=(c^2*k)/h^2;
+% x=-1:h:1;
+% t=0:k:1;
+% m=length(x);
+% n=length(t);
+% u=zeros(m,n);
+% IC=cos((pi*x)/2);
+% u(:,1)=IC';
+% u([1,end],:)=0;
+% for j=1:n-1
+%     for i=2:m-1
+%         u(i,j+1)=w*u(i+1,j)+(1-2*w)*u(i,j)+w*u(i-1,j);
+%     end
+% end
+% u
+% surf(u);
+% xlabel('x');
+% ylabel('t');
+% zlabel('u');
+% title('Heat Equation');
+% 
+% 
+% clear;clc;
+% h=input('enter value of h');
+% k=input('enter value of k');
+% c=input('enter value of c');
+% w=(c^2*k)/h^2;
+% x=0:h:1;
+% t=0:k:1;
+% m=length(x);
+% n=length(t);
+% u=zeros(m,n);
+% if (0<=x<=1/2)
+%     IC=x;
+% else
+%     IC=(1-x);
+% end
+% u(:,1)=IC';
+% u([1,end],:)=0;
+% for j=1:n-1
+%     for i=2:m-1
+%         u(i,j+1)=w*u(i+1,j)+(1-2*w)*u(i,j)+w*u(i-1,j);
+%     end
+% end
+% u
+% surf(u);
+% xlabel('x');
+% ylabel('t');
+% zlabel('u');
+% title('Heat Equation');
